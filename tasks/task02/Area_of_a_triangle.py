4469fb1f514b7156c6663c57283d62b04036df71
Valery Rak <vrak@exadel.com>
2021-09-16 11:42:03 +0300

    Task 02: Area of a triangle v1.1

--- tasks/task02/Area_of_a_triangle.py
+++ tasks/task02/Area_of_a_triangle.py
@@ -1,19 +1,21 @@
 # main menu
 import math
-print("Welcome to the triangle area calculation tool.")
+print("\nWelcome to the triangle area calculation tool.")
 while True:
-    print("\n","Menu:","1. Calculate triangle area by base and height","2. Calculate triangle area by 2 sides and angle between them","3. Exit", sep = "\n")
-    print("Enter menu item number: ", end = "")
-    mitem = input()
+    print("\nMenu:","1. Calculate triangle area by base and height","2. Calculate triangle area by 2 sides and angle between them","3. Exit", sep = "\n")
+    mitem = input("Enter menu item number: ")
     if mitem == "1":
-        print("Enter base and height (space separated): ", end = "")
+        print("\nEnter base and height (space separated): ", end = "")
         while True:
             inp = input().split(" ")
             try:
-                base, height = [float(x) for x in inp]
-                print("\nbase =", base, " height =", height) 
-                print("Area is: ", base * height / 2)
-                break
+                base, height = map(float, inp)
+                print(f"\nbase = {base}, height = {height}") 
+                if base > 0 and height > 0:
+                    print("Area is: ", base * height / 2)
+                    break
+                else:
+                    print("The side lengths of the triangle must be positive numbers. Please enter 2 positive numbers: ", end = "")
             except ValueError: #could not convert string to float or too many/few values to unpack
                 if len(inp) == 2:
                     err = "non-numeric"
@@ -21,14 +23,17 @@ while True:
                     err = len(inp)
                 print("\nYou entered ", err," value(s). Please enter 2 numeric values space separated: ", end = "")
     elif mitem == "2":
-        print("Enter 2 sides and angle(degrees) between them (space separated): ", end = "")
+        print("\nEnter 2 sides and angle(degrees) between them (space separated): ", end = "")
         while True:
             inp = input().split(" ")
             try:
-                side1, side2, angle = [float(x) for x in inp]
-                print("side1 =", side1, " side2 =", side2, " angle =", angle)
-                print("Area is: ", (side1 * side2 * math.sin(angle * math.pi / 180) / 2))
-                break
+                side1, side2, angle = map(float, inp)
+                print(f"\nside1 = {side1}, side2 = {side2}, angle = {angle}")
+                if side1 > 0 and side2 > 0 and angle > 0 and angle < 180:
+                    print("Area is: ", round((side1 * side2 * math.sin(math.radians(angle)) / 2), 2))
+                    break
+                else:
+                    print("The lengths of the sides of the triangle must be positive numbers, the angle between the sides of the triangle is greater than zero and less than 180 degrees. Please enter 3 numbers in valid ranges: ", end = "")
             except ValueError: #could not convert string to float or too many/few values to unpack
                 if len(inp) == 3:
                     err = "non-numeric"
